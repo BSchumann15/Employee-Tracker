8c0cb2128d14dd9636990f62b4499e94df39a9e7
Blake Schumann <blakeschumann15@gmail.com>
2021-08-30 19:09:22 +0930

    editing sql files

--- db/seed.sql
+++ db/seed.sql
@@ -1,38 +1,29 @@
-USE employees_db;
+INSERT INTO department (name)
+VALUES 
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
 
-INSERT INTO department (name)
-VALUES ("sales");
-INSERT INTO department (name)
-VALUES ("Engineering")
-INSERT INTO department (name)
-VALUES ("legal");
-INSERT INTO department (name)
-VALUES ("finance");
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 80000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
 
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Bruce", "Wayne",1 ,3);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Barry", "Allen",2 ,1);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Clark", "Kent",3 ,NULL);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Diana", "Prince",4 ,3);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Arthur", "Curry",5 ,NULL);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Kendra", "Saunders",2 ,NULL);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Dinah", "Drake",4 ,7);
-INSERT INTO employee (firstName, lastName, role_id, manager_id)
-VALUES ("Hal", "Jordan",1 ,2);
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Accountant", 125000, 3);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Mark', 'Miller', 2, null),
+('Devin', 'Anderson', 1, 1),
+('Mary', 'Brown', 4, null),
+('Ashley', 'Jones', 3, 3),
+('Tyler', 'Moore', 6, null),
+('Ana', 'Sanchez', 5, 5),
+('Lewis', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
